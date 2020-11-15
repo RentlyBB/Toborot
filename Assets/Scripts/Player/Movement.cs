f1dd666c0cba9d5882a88bfdc0a63acb0e9a77f6
RentlyBB <patrikchleboun8@gmail.com>
2020-11-15 16:04:11 +0100

    Uprava kamery, testovaci level

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,19 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Movement : MonoBehaviour{
 
-    public float speed = 100f;
+    public float movementSpeed = 20f;
 
     public LayerMask layerMask;
-
-    public Vector3 pos;
+    Vector3 nextPos;
 
     // Start is called before the first frame update
     void Start(){
         lineUpPosition();
-        pos = transform.position;
     }
 
     private void FixedUpdate() {
@@ -23,41 +22,46 @@ public class Movement : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
+        toEdgeMovemet();
+    }
 
-        if(transform.position == pos){
-            if(Input.GetKeyDown(KeyCode.W)) {
-                pos += new Vector3(0, calculateRayCastDistance(Vector2.up), 0);
-            } else if(Input.GetKeyDown(KeyCode.A)) {
-                pos -= new Vector3(calculateRayCastDistance(Vector2.left), 0, 0);
-            } else if(Input.GetKeyDown(KeyCode.S)) {
-                pos -= new Vector3(0, calculateRayCastDistance(Vector2.down), 0);
-            } else if(Input.GetKeyDown(KeyCode.D)) {
-                pos += new Vector3(calculateRayCastDistance(Vector2.right), 0, 0);
+    void toEdgeMovemet(){
+        if(transform.position == nextPos) {
+            if(Input.GetKey(KeyCode.W)) {
+                nextPos += new Vector3(0, calculateRayCastDistance(Vector2.up), 0);
+            } else if(Input.GetKey(KeyCode.S)) {
+                nextPos -= new Vector3(0, calculateRayCastDistance(Vector2.down), 0);
+            } else if(Input.GetKey(KeyCode.A)) {
+                nextPos -= new Vector3(calculateRayCastDistance(Vector2.left), 0, 0);
+            } else if(Input.GetKey(KeyCode.D)) {
+                nextPos += new Vector3(calculateRayCastDistance(Vector2.right), 0, 0);
             }
         }
-
-     
-      transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * movementSpeed);
     }
 
     //Vypocita vzdalenost od hrace k nejblizsimu stene v dannem smeru 
     float calculateRayCastDistance(Vector2 dir){
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 500, ~layerMask);
-        return hit.distance - 0.5f;
+        return Physics2D.Raycast(transform.position, dir, 500, ~layerMask).distance - 0.5f;
+    }
+  
+    void lineUpPosition(){
+        transform.position = new Vector3((float)Math.Ceiling(transform.position.x) - 0.5f, (float)Math.Ceiling(transform.position.y) - 0.5f, 0);
+        nextPos = transform.position;
     }
 
-
-    void castAndRenderRayCast(){
+    void castAndRenderRayCast() {
         //Vykresleni raycastu
         Debug.DrawRay(transform.position, new Vector3(0, calculateRayCastDistance(Vector2.up), 0), Color.blue);
         Debug.DrawRay(transform.position, new Vector3(-calculateRayCastDistance(Vector2.left), 0, 0), Color.blue);
         Debug.DrawRay(transform.position, new Vector3(0, -calculateRayCastDistance(Vector2.down), 0), Color.blue);
         Debug.DrawRay(transform.position, new Vector3(calculateRayCastDistance(Vector2.right), 0, 0), Color.blue);
     }
+}
 
-    void lineUpPosition(){
-        transform.position = new Vector3((float)Math.Ceiling(transform.position.x) - 0.5f, (float)Math.Ceiling(transform.position.y) - 0.5f, 0);
+[CustomEditor(typeof(Movement))]
+public class MovementEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
     }
-
-
 }
